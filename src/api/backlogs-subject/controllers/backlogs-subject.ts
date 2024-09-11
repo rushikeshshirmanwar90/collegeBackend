af3c1dca40fad05a5b85e23c0167cebe8ef659d5
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-09-11 13:52:47 +0530

    some changes

---
+++ src/api/backlogs-subject/controllers/backlogs-subject.ts
@@ -0,0 +1,7 @@
+/**
+ * backlogs-subject controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::backlogs-subject.backlogs-subject');
---
+++ src/api/wiselist/services/wiselist.ts
@@ -0,0 +1,7 @@
+/**
+ * wiselist service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::wiselist.wiselist');
